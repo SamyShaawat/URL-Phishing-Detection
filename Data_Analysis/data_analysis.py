13bd5840b4ab95c38272ddf3384ec2ab17e5469b
SamyShaawat <samy.shaawat@gmail.com>
2023-12-10 01:48:09 +0200

    represent commit

--- Data_Analysis/data_analysis.py
+++ Data_Analysis/data_analysis.py
@@ -1,3 +1,4 @@
+from sys import displayhook
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,20 +7,27 @@ def analyze_dataset(file_path):
     # Read the dataset
     df = pd.read_csv(file_path)
 
+    # Set display options for pandas
+    pd.set_option("display.max_columns", None)  # Show all columns
+    pd.set_option("display.width", 1000)  # Set display width
+
     # Display the number of rows and columns
     print(f"Number of rows: {df.shape[0]}")
     print(f"Number of columns: {df.shape[1]}")
 
-    # Display the first few rows
-    print("Head of the dataset:")
-    print(df.head())
+    # Display the first few rows with borders
+    displayhook(
+        df.head().style.set_table_styles(
+            [
+                {"selector": "th", "props": [("border", "2px solid white")]},
+                {"selector": "td", "props": [("border", "2px solid white")]},
+            ]
+        )
+    )
 
     # Analyze and plot the 'label' column if it exists
     if "label" in df.columns:
         label_counts = df["label"].value_counts()
-        print("Counts in 'label' column:")
-        print(label_counts)
-
         # Get the number of good and bad URLs
         num_good = label_counts.get("good", 0)  # Default to 0 if 'good' is not a key
         num_bad = label_counts.get("bad", 0)  # Default to 0 if 'bad' is not a key
@@ -38,9 +46,3 @@ def analyze_dataset(file_path):
 
 # Example usage
 # analyze_dataset('path_to_your_dataset.csv')
-
-
-import os
-
-os.chdir("/path/to/directory")
-from data_analysis import analyze_dataset
