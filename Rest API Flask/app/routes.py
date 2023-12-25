d718998bb966aca99f6375909b127cdd14c5c2c9
SamyShaawat <samy.shaawat@gmail.com>
2023-12-25 13:10:52 +0200

    routes.py modify commit

--- Rest API Flask/app/routes.py
+++ Rest API Flask/app/routes.py
@@ -9,8 +9,8 @@ routes_blueprint = Blueprint("routes", __name__)
 @routes_blueprint.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-    text = data["text"]
-    inputs = preprocess_input(text, tokenizer)
+    url = data["URL"]
+    inputs = preprocess_input(url, tokenizer)
 
     with torch.no_grad():
         outputs = model(**inputs)
