d0bbe89dbfcff5e01bb1081705f89e8b439305a9
SamyShaawat <samy.shaawat@gmail.com>
2023-12-29 13:02:05 -0800

    shuffling commit

--- Rest API Flask/app/utils.py
+++ Rest API Flask/app/utils.py
@@ -3,7 +3,8 @@ def preprocess_input(text, tokenizer, max_length=128):
         text,
         add_special_tokens=True,
         max_length=max_length,
-        pad_to_max_length=True,
+        padding="max_length",  # Changed from pad_to_max_length
+        truncation=True,  # Explicitly enable truncation
         return_attention_mask=True,
         return_tensors="pt",
     )
