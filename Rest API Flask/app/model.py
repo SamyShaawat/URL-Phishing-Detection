9674008261eca9681987ba84b8ed1fd5e9512519
SamyShaawat <samy.shaawat@gmail.com>
2024-01-22 19:18:07 -0800

    final commit

--- Rest API Flask/app/model.py
+++ Rest API Flask/app/model.py
@@ -3,9 +3,9 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 def load_model():
     model = AutoModelForSequenceClassification.from_pretrained(
-        "./models/savedModel_11430"
+        "./models/savedModel_20000"
     )
-    tokenizer = AutoTokenizer.from_pretrained("./models/savedTokenizer_11430")
+    tokenizer = AutoTokenizer.from_pretrained("./models/savedTokenizer_20000")
     return model, tokenizer
 
 
